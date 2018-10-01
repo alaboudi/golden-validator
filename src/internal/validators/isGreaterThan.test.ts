af1188e06452e48e265b6ca5745f8107bc839e83
Yazan Alaboudi <yazan.alaboudi@gmail.com>
2018-10-01 09:54:20 -0400

    create isGreaterThan tests

--- src/internal/validators/isGreaterThan.test.ts
+++ src/internal/validators/isGreaterThan.test.ts
@@ -1,24 +1,21 @@
-import {
-    createIsGreaterThanEvaluator,
-    createIsGreaterThanErrorMessage
-} from './isGreaterThan';
+import { createIsGreaterThanErrorMessage, createIsGreaterThanEvaluator } from './isGreaterThan';
 
 describe('createIsGreaterThanEvaluator', () => {
-    const constraint = 5;
-    const isGreaterThanEvaluator = createIsGreaterThanEvaluator(constraint);
-    it('should return an evaluator function loaded with a constraint when invoked with 1 parameter', () => {
-        expect(isGreaterThanEvaluator).toBeInstanceOf(Function);
-    });
-    it('should return true when supplied with a value greater than a loaded constraint', () => {
-        expect(isGreaterThanEvaluator(10)).toBe(true);
-    });
-    it('should return true when supplied with a value greater than a loaded constraint', () => {
-        expect(isGreaterThanEvaluator(4)).toBe(false);
-    });
+  const constraint = 5;
+  const isGreaterThanEvaluator = createIsGreaterThanEvaluator(constraint);
+  it('should return an evaluator function loaded with a constraint when invoked with 1 parameter', () => {
+    expect(isGreaterThanEvaluator).toBeInstanceOf(Function);
+  });
+  it('should return true when supplied with a value greater than a loaded constraint', () => {
+    expect(isGreaterThanEvaluator(10)).toBe(true);
+  });
+  it('should return true when supplied with a value greater than a loaded constraint', () => {
+    expect(isGreaterThanEvaluator(4)).toBe(false);
+  });
 });
 
 describe('createIsGreaterThanErrorMessage', () => {
-    it('should generate the default error message', () => {
-        expect(createIsGreaterThanErrorMessage(5)).toBe('property is not greater than 5');
-    });
+  it('should generate the default error message', () => {
+    expect(createIsGreaterThanErrorMessage(5)).toBe('property is not greater than 5');
+  });
 });
