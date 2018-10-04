34590364fe670d77e5dcdd9b8957c5c107ff952d
Yazan Alaboudi <yazan.alaboudi@gmail.com>
2018-10-04 03:42:09 -0400

    update variable naming convention in isGreaterThan

--- src/internal/validators/isGreaterThan.ts
+++ src/internal/validators/isGreaterThan.ts
@@ -1,7 +1,7 @@
 import { createValidatorFactory } from '../factories';
 import { ErrorMessageFactory, EvaluatorFactory, ValidatorFactory } from '../types';
 
-export const createIsGreaterThanEvaluator: EvaluatorFactory = constraint => input => input > constraint;
+export const createIsGreaterThanEvaluator: EvaluatorFactory = constraint => value => value > constraint;
 export const createIsGreaterThanErrorMessage: ErrorMessageFactory = constraint =>
   `property is not greater than ${constraint}`;
 
