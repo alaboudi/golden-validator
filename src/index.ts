dd60e13153d732f2b1214d855e9220a34c9d4e93
Yazan Alaboudi <yazan.alaboudi@gmail.com>
2018-10-04 06:10:58 -0400

    expose createRule, createSchema, and isValid for end use

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
+/* Factory functions */
+export { createRule, createSchema } from './internal/factories';
+
 /* Validator Creators */
 export { isGreaterThan } from './internal/validators/isGreaterThan';
 export { isLessThan } from './internal/validators/isLessThan';
 export { isBetween } from './internal/validators/isBetween';
+
+/* resulters */
+export { isValid } from './internal/isvalid';
