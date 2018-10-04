fb6e9b67332dc9feb7eac340cdb11657f0e82db0
Yazan Alaboudi <yazan.alaboudi@gmail.com>
2018-10-04 04:36:37 -0400

    added a main file to the library

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+/* Validator Creators */
+export { isGreaterThan } from './internal/validators/isGreaterThan';
+export { isLessThan } from './internal/validators/isLessThan';
+export { isBetween } from './internal/validators/isBetween';
