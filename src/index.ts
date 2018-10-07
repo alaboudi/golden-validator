ac254f097dd8450a77b3be7e211d2d0b8bbb674f
Yazan Alaboudi <yazan.alaboudi@gmail.com>
2018-10-07 17:02:27 -0400

    :sparkles: expose isEmail, isFalsy, isNumber, isTruthy

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ export { isGreaterThan } from './internal/validators/isGreaterThan';
 export { isLessThan } from './internal/validators/isLessThan';
 export { isBetween } from './internal/validators/isBetween';
 export { isEqualTo } from './internal/validators/isEqualTo';
+export { isEmail } from './internal/validators/isEmail';
+export { isFalsy } from './internal/validators/isFalsy';
+export { isNumber } from './internal/validators/isNumber';
+export { isTruthy } from './internal/validators/isTruthy';
 
 /* resulters */
 export { isValid } from './internal/isvalid';
