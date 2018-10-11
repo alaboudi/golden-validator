0227b87c20bbd16a44621f1dbc4b1392f17fc86b
Yazan Alaboudi <yazan.alaboudi@gmail.com>
2018-10-11 18:47:32 -0400

    :zap: improving performance of doesValuePassRule and isValid functions (#3)

--- src/internal/isvalid.ts
+++ src/internal/isvalid.ts
@@ -4,11 +4,11 @@ export const doesValuePassRule = (value: any, rule: IRule): boolean => {
   if (value === undefined || value === null) {
     return !rule.required;
   }
-  return rule.validators.reduce((acc, validator) => acc && validator.evaluator(value), true);
+  return !rule.validators.some(validator => !validator.evaluator(value));
 };
 
 export const isValid = <T>(model: T, schema: ISchema<T>): boolean => {
   const rules = schema.rules;
   const keys = Object.keys(schema.rules) as Array<keyof T>;
-  return keys.reduce((acc, key) => doesValuePassRule(model[key], rules[key]) && acc, true);
+  return !keys.some(key => !doesValuePassRule(model[key], rules[key]));
 };
